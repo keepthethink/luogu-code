71b0f21e79d3e6a1e84f1b28c86e266727d4d6d6
keepthethink <keepthethink@hotmail.com>
2019-06-24 10:50:14 +0800

    create P2678

---
+++ P2678/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  P2678
+//
+//  Created by Kanger He on 2019/6/24.
+//  Copyright © 2019 Kanger He. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    
+    return 0;
+}
